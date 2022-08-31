267ff4a1e0651ee3f19cd60ef2d202b35a9df6b1
emonmeena <mayank16meena@gmail.com>
2022-08-31 13:05:58 +0530

    final push

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import {
   Routes,
   Route,
   Link,
+  NavLink,
   useParams,
 } from "react-router-dom";
 import Web3 from "web3";
 import Home from "./pages/Home";
 import "./App.css";
 import Navbar from "./components/navbar/Navbar";
-import { Button } from "react-bootstrap";
+import { Button, Modal } from "react-bootstrap";
 
 export default function App(props) {
   const [account, setAccount] = useState("");
@@ -20,6 +21,12 @@ export default function App(props) {
 
   const [tokenId, settokenId] = useState();
   const [owner, setowner] = useState();
+  const [show, setShow] = useState(false);
+  const [eventName, setEventName] = useState("");
+  const [totalSupply, setTotalSupply] = useState();
+  const [basePrice, setBasePrice] = useState();
+  const [ev, setEv] = useState([1,2]);
+  const [ot, setOt] = useState([]);
 
   useEffect(() => {
     console.log("useEffect");
@@ -44,6 +51,17 @@ export default function App(props) {
         "Non-Ethereum browser detected. You should consider trying MetaMask!"
       );
     }
+  };
+
+  const createEvent = () => {
+    console.log("creating event");
+    // createNewEvent(eventName, totalSupply, basePrice);
+    setEv((ev)=> [...ev, ev.length + 1])
+    setShow(false);
+  };
+
+  const handleClose = () => {
+    setShow(false);
   };
 
   const loadBlockchainData = async () => {
@@ -906,11 +924,6 @@ export default function App(props) {
           type: "function",
         },
       ],
-      // 0xC37d1D95A017EBfb54b590BA0BF22dE26060a99A
-      // 0x7d9916f76FB9D48D37A1710C5e49D24b7e1a3eEd
-      // "0x44E55DbCa6672b6CbE7419bDEFdC2997E145Fb5c",
-      // "0x055e43aFd89e4722bC49d38951E1b523c348beE9",
-      // "0x7CED9f328E27C51f9d01D90C50027eC3a90b9e40",
       "0x410353A524d1AabD1A8E5aaCf66c18b318842c92",
       { from: account, gas: 150000, gasPrice: "30000000000" }
     );
@@ -1132,66 +1145,334 @@ export default function App(props) {
       .catch((err) => console.log(err));
   };
 
+  const H = () => {
+    return <div>Home</div>;
+  };
+
   const Main = () => {
-    return loading ? (
-      <div
-        id="loader"
-        className="text-center d-flex align-items-center w-100 justify-content-center fs-4"
-        style={{ height: "70vh", fontWeight: "600" }}
-      >
-        <p className="text-center">Loading...</p>
-      </div>
-    ) : (
-      <>
-        <Home
-          account={account}
-          createNewEvent={createNewEvent}
-          openEventSale={openEventSale}
-          getAvailableEvents={getAvailableEvents}
-          eventsData={eventsData}
-          closeEventSale={closeEventSale}
-          URImintForEventId={URImintForEventId}
-          BulkURImintForEventId={BulkURImintForEventId}
-          AuthorizeSellTicketfromEventOwner={AuthorizeSellTicketfromEventOwner}
-          sellToken={sellToken}
-          bid={bid}
-          releaseTokentoHighestBidder={releaseTokentoHighestBidder}
-        />
-        <div>
-          <div>
-            <input
-              value={tokenId}
-              onChange={(e) => settokenId(e.target.value)}
-              type="text"
-              placeholder="tokenId"
-              className="col-1"
-            />
-            <Button onClick={() => ownerOf(tokenId)}>ownerOf</Button>
+    return (
+      <Router>
+        <Navbar account={account} />
+        <div className="row">
+          <div className="col-2 border vh-100">
+            <div>
+              <NavLink className="d-block m-2" to="/">
+                Available Events
+              </NavLink>
+              <NavLink
+                className="d-block m-2"
+                to={`ticketsboughtby/${account}`}
+              >
+                Your Tickets
+              </NavLink>
+            </div>
+            <div>
+              <Button className="mx-2" onClick={() => setShow(true)}>
+                Add New Event
+              </Button>
+            </div>
           </div>
-          {owner}
-          <div className="d-flex"></div>
-          <div className="d-flex my-2"></div>
+          <div className="col-10">
+            <Routes>
+              <Route path="/" element={<Events eventsData1={ev} />}></Route>
+              <Route path="event" element={<Events />}>
+                <Route path=":eventid" element={<EventPage />} />
+              </Route>
+              <Route path="ticketsboughtby" element={<OwnersTicket />}>
+                <Route path=":owneraddress" element={<OwnersTicket eventsData={ot}/>} />
+              </Route>
+              <Route path="tickets" element={<TicketPage />}>
+                <Route path=":ticketid" element={<TicketPage />} />
+              </Route>
+              <Route path="ticketsofevent" element={<EventsTicket />}>
+                <Route path=":eventid" element={<EventsTicket />} />
+              </Route>
+              <Route
+                path="auctionforevent/:eventid"
+                element={<AuctionTicket />}
+              />
+            </Routes>
+          </div>
+          <div>
+            <Modal show={show} onHide={handleClose}>
+              <Modal.Header closeButton>
+                <Modal.Title>Add New Event</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                <form>
+                  <div className="form-group mx-2">
+                    <label htmlFor="EventName">Event Name</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="EventName"
+                      aria-describedby="emailHelp"
+                      placeholder="Event Name"
+                      value={eventName}
+                      onChange={(e) => setEventName(e.target.value)}
+                    />
+                  </div>
+                  <div className="form-group mx-2">
+                    <label htmlFor="totalSupply">Total Supply</label>
+                    <input
+                      type="number"
+                      className="form-control"
+                      id="totalSupply"
+                      placeholder="100"
+                      value={totalSupply}
+                      onChange={(e) => setTotalSupply(e.target.value)}
+                    />
+                  </div>
+                  <div className="form-group mx-2">
+                    <label htmlFor="basePrice">Base Price</label>
+                    <input
+                      type="number"
+                      className="form-control"
+                      id="basePrice"
+                      placeholder="100"
+                      value={basePrice}
+                      onChange={(e) => setBasePrice(e.target.value)}
+                    />
+                  </div>
+                </form>
+              </Modal.Body>
+              <Modal.Footer>
+                <Button variant="secondary" onClick={handleClose}>
+                  Close
+                </Button>
+                <Button variant="primary" onClick={() => createEvent()}>
+                  Create
+                </Button>
+              </Modal.Footer>
+            </Modal>
+          </div>
+        </div>
+      </Router>
+    );
+  };
+
+  const Events = ({eventsData1}) => {
+    // let eventsData1 = [1, 2, 3];
+    return (
+      <>
+        <div className="px-5 row pt-5">
+          {eventsData1.map((item) => {
+            return (
+              <div
+                key={item.eventId + item.eventName}
+                className="col-3 border m-1 mx-2 d-flex justify-content-center"
+              >
+                <div>
+                  <img
+                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4UwTtZRMBCq2W718Ym4Unt66e9eT9KT5Icg&usqp=CAU"
+                    alt="NFT"
+                    width="300"
+                    height="300"
+                  />
+                  <div className="pt-2">
+                    {/* <p>Event ID: {item.eventId}</p>
+                    <p className="bold">Event Name: {item.eventName}</p>
+                    <p>Total Supply: {item.totalSupply}</p>
+                    <p>Base Price: {item.basePrice}</p>
+                    <p>EventOwner: {item.EventOwner}</p>
+                    <p>Available Tickets: {item.availableTickets}</p>
+                    <p>Minted Tickets: {item.mintedTickets}</p>
+                    <p>isSaleActive: {item.saleIsActive ? "True" : "False"}</p> */}
+                    <p>Event {item}</p>
+                    <NavLink
+                      className="d-block m-2"
+                      to={`ticketsofevent/${item}`}
+                    >
+                      Buy Tickets
+                    </NavLink>
+                    <NavLink
+                      className="d-block m-2"
+                      to={`auctionforevent/${item}`}
+                    >
+                      Buy in Auction
+                    </NavLink>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
         </div>
       </>
     );
   };
 
-  return (
-    <>
-      <Router>
-        <Navbar account={account} />
-        <Routes>
-          <Route path="/" element={<Main />}></Route>
-          <Route path="event" element={<EventPage />}>
-            <Route path=":eventid" element={<EventPage />} />
-          </Route>
-        </Routes>
-      </Router>
-    </>
+  return loading ? (
+    <div
+      id="loader"
+      className="text-center d-flex align-items-center w-100 justify-content-center fs-4"
+      style={{ height: "70vh", fontWeight: "600" }}
+    >
+      <p className="text-center">Loading...</p>
+    </div>
+  ) : (
+    <Main />
   );
 }
 
 const EventPage = () => {
   let params = useParams();
-  return <h2>Event: {params.eventid}</h2>;
+  return (
+    <>
+      <h2>Event: {params.eventid}</h2>
+      <div className="col-3 border m-1 mx-2 d-flex justify-content-center">
+        <div>
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4UwTtZRMBCq2W718Ym4Unt66e9eT9KT5Icg&usqp=CAU"
+            alt="NFT"
+            width="300"
+            height="300"
+          />
+          {/* <div className="pt-2">
+                  <p>Event ID: {item.eventId}</p>
+                  <p className="bold">Event Name: {item.eventName}</p>
+                  <p>Total Supply: {item.totalSupply}</p>
+                  <p>Base Price: {item.basePrice}</p>
+                  <p>EventOwner: {item.EventOwner}</p>
+                  <p>Available Tickets: {item.availableTickets}</p>
+                  <p>Minted Tickets: {item.mintedTickets}</p>
+                  <p>isSaleActive: {item.saleIsActive ? "True" : "False"}</p>
+                </div> */}
+          <div>
+            event
+            <Button variant="secondary">Buy</Button>
+            <p>$500</p>
+            {/* <Button variant="secondary">Close</Button> */}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+const EventsTicket = () => {
+  let params = useParams();
+  let eventsData = [1, 2];
+  return (
+    <>
+      <h2>Tickets of the Event: {params.eventid}</h2>
+      <div className="px-5 row pt-5 col-12">
+        {eventsData.map((item) => {
+          return (
+            <div
+              key={item.eventId + item.eventName}
+              className="col-3 border m-1 mx-2 d-flex justify-content-center"
+            >
+              <div>
+                <img
+                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4UwTtZRMBCq2W718Ym4Unt66e9eT9KT5Icg&usqp=CAU"
+                  alt="NFT"
+                  width="300"
+                  height="300"
+                />
+                {/* <div className="pt-2">
+                  <p>Event ID: {item.eventId}</p>
+                  <p className="bold">Event Name: {item.eventName}</p>
+                  <p>Total Supply: {item.totalSupply}</p>
+                  <p>Base Price: {item.basePrice}</p>
+                  <p>EventOwner: {item.EventOwner}</p>
+                  <p>Available Tickets: {item.availableTickets}</p>
+                  <p>Minted Tickets: {item.mintedTickets}</p>
+                  <p>isSaleActive: {item.saleIsActive ? "True" : "False"}</p>
+                </div> */}
+                <p>ticket {item}</p>
+                <Button className="mx-2" onClick>Buy Now</Button>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+};
+
+const AuctionTicket = () => {
+  let params = useParams();
+  let eventsData = [1, 2];
+  return (
+    <>
+      <h2>Auction tickets of the Event: {params.eventid}</h2>
+      <div className="px-5 row pt-5 col-12">
+        {eventsData.map((item) => {
+          return (
+            <div
+              key={item.eventId + item.eventName}
+              className="col-3 border m-1 mx-2 d-flex justify-content-center"
+            >
+              <div>
+                <img
+                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4UwTtZRMBCq2W718Ym4Unt66e9eT9KT5Icg&usqp=CAU"
+                  alt="NFT"
+                  width="300"
+                  height="300"
+                />
+                {/* <div className="pt-2">
+                  <p>Event ID: {item.eventId}</p>
+                  <p className="bold">Event Name: {item.eventName}</p>
+                  <p>Total Supply: {item.totalSupply}</p>
+                  <p>Base Price: {item.basePrice}</p>
+                  <p>EventOwner: {item.EventOwner}</p>
+                  <p>Available Tickets: {item.availableTickets}</p>
+                  <p>Minted Tickets: {item.mintedTickets}</p>
+                  <p>isSaleActive: {item.saleIsActive ? "True" : "False"}</p>
+                </div> */}
+                <p>ticket {item}</p>
+                <Button className="d-block m-2">Bid</Button>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+};
+
+const TicketPage = () => {
+  let params = useParams();
+  return <h2>TTicketID: {params.owneraddress}</h2>;
+};
+
+const OwnersTicket = ({}) => {
+  let params = useParams();
+  let eventsData = [1, 2, 3];
+  return (
+    <>
+      <h2>Tickets of the owner: {params.owneraddress}</h2>
+      <div className="px-5 row pt-5 col-12">
+        {eventsData.map((item) => {
+          return (
+            <div
+              key={item.eventId + item.eventName}
+              className="col-3 border m-1 mx-2 d-flex justify-content-center"
+            >
+              <div>
+                <img
+                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4UwTtZRMBCq2W718Ym4Unt66e9eT9KT5Icg&usqp=CAU"
+                  alt="NFT"
+                  width="300"
+                  height="300"
+                />
+                {/* <div className="pt-2">
+                  <p>Event ID: {item.eventId}</p>
+                  <p className="bold">Event Name: {item.eventName}</p>
+                  <p>Total Supply: {item.totalSupply}</p>
+                  <p>Base Price: {item.basePrice}</p>
+                  <p>EventOwner: {item.EventOwner}</p>
+                  <p>Available Tickets: {item.availableTickets}</p>
+                  <p>Minted Tickets: {item.mintedTickets}</p>
+                  <p>isSaleActive: {item.saleIsActive ? "True" : "False"}</p>
+                </div> */}
+                <p>ticket {item}</p>
+                <Button>Sell in Auction</Button>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
 };
